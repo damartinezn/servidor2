9f15ef4f6e163c5b3eabf686153b5f7b5c459e38
damartinezn <damartinezn@uce.edu.ec>
2019-11-29 15:53:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -13,25 +13,6 @@ app.get('/hola',(req,res)=>{
 	res.send({message: "hola mundo"})
 })
 
-app.get('/api/product',(req, res) =>{
-	res.send(200, {product: []})
-})
-
-app.get('/api/product/:productid',(req, res)=>{
-
-})
-
-app.post('/api/product',(req,res)=>{
-	console.log(req.body)
-	res.status(200).send({message: 'El producto se ha recibido'})
-})
-
-app.put('/api/product/:productId',(req,res)=>{
-
-})
-app.delete('/api/product/:productId',(req,res)=>{
-
-})
 
 app.listen(port, ()=>{
 	console.log('API REST corriendo en http://localhost:${port}')
